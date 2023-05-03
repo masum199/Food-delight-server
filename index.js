aba72ce3e8bea9cf6f62fd9cb1ec73dc0d3a0488
unknown <24abdullahalmasum@gmail.com>
2023-05-03 16:39:17 +0600

    added data2

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const app = express();
 const cors = require('cors');
 const port =process.env.PORT || 5000;
 
+const chefData = require('./data/data.json')
+
 app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('yoo server is running');
+})
+
+app.get('/chefData',(req, res) => {
+    res.send(chefData)
 })
 
 app.listen(port, () => {
